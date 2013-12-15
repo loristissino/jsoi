7c4cd5ac3e67a985f698d15aadd6f5a60f1bb6de
Loris Tissino <loris.tissino@gmail.com>
2013-12-15 16:07:59 +0100

    Fix: changed name of file to myapp.html

--- utilities/preparezip.sh
+++ utilities/preparezip.sh
@@ -4,7 +4,7 @@ cd /var/www/ojsi
 mkdir ojsi
 cd ojsi
 wget 127.0.0.1/ojsi/index.php -O index.html
-wget 127.0.0.1/ojsi/alone.php -O alone.html
+wget 127.0.0.1/ojsi/myapp.php -O myapp.html
 cp -r ../resources .
 cd ..
 
